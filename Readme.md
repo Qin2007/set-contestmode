3c31817258c66596ec4fba05f4311ef52c8613fc
qin2007 <qin@ractoc.com>
2025-05-22 13:36:40 +0200

    first update

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# [set-contestmode](https://developers.reddit.com/r/vast_attention/apps/set-contestmode)
+
+a hacky annoying solution to turn on contest mode on posts
+
+to use it simply select the menu option on a post and let it work.
+
+important: automoderator config must be made as a wiki page for this bot to work.
+if you have nothing to insert then `---` is enough to get started
+
+## internals, important maybe.
+
+when a moderator selects the menu option, the bot will go into the automoderator config and pastes
+
+```yaml
+---
+# set-contestmode
+type: comment
+author:
+    name: set-contestmode
+parent_submission:
+    set_contest_mode: true
+    action_reason: u/Vast_Attention595 turned on contest mode.
+action_reason: u/Vast_Attention595 turned on contest mode.
+body (regex, full-exact): "u/[a-z0-9\\-_]+ just enabled contest mode on your post"
+action: remove
+
+---
+```
+
+in it, where `u/Vast_Attention595` is the username of the initiator.
+then leaves a top level comment to have automoderator enable contest mode.
+
---
+++ src/main.tsx
@@ -0,0 +1,48 @@
+// Visit developers.reddit.com/docs to learn Devvit!
+
+import { Devvit, RedditAPIClient, CreateWikiPageOptions, WikiPage } from '@devvit/public-api';
+
+Devvit.configure({
+  redditAPI: true,
+});
+
+const COMMENT_JOB_NAME = 'delayed_comment';
+
+Devvit.addSchedulerJob({
+  name: COMMENT_JOB_NAME,
+  onRun: async (event, context) => {
+    const { id, commentBody } = event.data!, text = commentBody;
+    if (typeof id !== 'string' || typeof text !== 'string') return;
+    await context.reddit.submitComment({ id, text, });
+  },
+});
+
+Devvit.addMenuItem({
+  location: 'post',
+  label: 'Enable ContestMode',
+  forUserType: 'moderator',
+  onPress: async function (event, context) {
+    context.ui.showToast('Hello!');
+    const userId = context.userId, wikipageName = 'config/automoderator'; let user, username;
+    if (userId !== undefined) { user = await context.reddit.getUserById(userId); username = user?.username; } else return;
+    if (username === undefined) return;// weird i can fetch a user without name
+    const postId = event.targetId, subreddit = await context.reddit.getCurrentSubreddit(), reddit = context.reddit;
+    const wikiPage = await reddit.getWikiPage(subreddit.name, wikipageName), content: string = wikiPage.content;
+    const automoderatorAddition = '\n\n---\n# set-contestmode\ntype: comment\nauthor:\n    name: set-contestmode'
+      + '\nparent_submission:\n    set_contest_mode: true\n    action_reason: u/' + username + ' turned on contest' +
+      ' mode.\naction_reason: u/' + username + ' turned on contest mode.\nbody (regex, full-exact): "u/[a-z0-9\\\\-_]+' +
+      ` just enabled contest mode on your post"\nmoderators_exempt: false\naction: remove`,
+      commentBody = `u/${username} just enabled contest mode on your post`;
+    await reddit.updateWikiPage({
+      subredditName: subreddit.name,
+      page: wikipageName, content: content + automoderatorAddition,
+      reason: 'action_reason: u/' + username + ' turned on contest mode.',
+    });
+    await context.scheduler.runJob({
+      name: COMMENT_JOB_NAME, data: { id: postId ?? null, commentBody },
+      runAt: new Date(Date.now() + 2500), // 3 seconds from now
+    });
+  },
+});
+
+export default Devvit;
